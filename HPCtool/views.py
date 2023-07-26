bde682259dab4d01515d6485b9be46132e2a746e
0pss <71946340+0pss@users.noreply.github.com>
2023-07-26 15:40:40 +0200

    Ajax call from Browser working

--- HPCtool/views.py
+++ HPCtool/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.generic import TemplateView
+
+from django_mapengine import views
+
 
 # Create your views here.
+class HomePageView(TemplateView, views.MapEngineMixin):
+
+    template_name = "minimal_mapengine.html"
+
+   # def get(self, request, **kwargs):
+   #     return render(request, self.template_name)
+
+    def post(self, request, **kwargs):
+        print("POST POST PSOT", request.POST)
+
+        response = JsonResponse({"error": "there was an error",
+                                 "message": "yeeeey"})
+        response.status_code = 400 #oder 200 # To announce that the user isn't allowed to publish
+        return response
+
+        #return HttpResponse("{'result': 'IS_PASS'}", content_type="application/json") #render(request, self.template_name)
--- ebus_map/urls.py
+++ ebus_map/urls.py
@@ -4,14 +4,15 @@
 from django.urls import path
 
 from . import views
+from HPCtool.views import HomePageView
 
 app_name = "map"
 
 urlpatterns = [
     # path("", views.MapGLView.as_view(), name="map"),
     # path("", views.MapGLView.as_view(), name="map"),
-    path("minimal", views.MinimalMapengineView.as_view(), name="minimal"),
-
+    ##path("minimal", views.MinimalMapengineView.as_view(), name="minimal"),
+    path("minimal", HomePageView.as_view(), name="minimal"),
     # path("choropleth/<str:lookup>/<str:scenario>", views.get_choropleth, name="choropleth"),
     # path("visualization", views.get_visualization, name="visualization"),
     path("popup/<str:lookup>/<int:id>", views.get_popup, name="popup"),
