cea277fc996332e4b7c106bde4b094cf2a3e63bf
PaulScheer <paul.scheer@rl-institut.de>
2023-08-01 09:01:12 +0200

    Merge Electrified station into station

--- ebustoolbox/models.py
+++ ebustoolbox/models.py
@@ -12,8 +12,8 @@ from ebus_map.managers import MVTManager, X, Y
 
 class Scenario(models.Model):
     name = models.CharField(max_length=100, blank=False)
-    opp_charging_power = models.FloatField(default=None)
-    dep_charging_power = models.FloatField(default=None)
+    opps_charging_power = models.FloatField(default=None, null=True, blank=True)
+    deps_charging_power = models.FloatField(default=None, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     task_id = models.TextField(default=None, null=True, blank=True)
     finished = models.DateTimeField(default=None, null=True, blank=True)
@@ -95,6 +95,7 @@ class VehicleClass(models.Model):
 
 class VehicleType(models.Model):
     name = models.CharField(max_length=100, blank=False)
+    scenario = models.ForeignKey(Scenario, on_delete=models.CASCADE)
     vehicle_class = models.ForeignKey(VehicleClass, on_delete=models.CASCADE)
     flex_charging = models.BooleanField()
     battery_capacity = models.FloatField()
@@ -109,15 +110,20 @@ class VehicleType(models.Model):
     v2g = models.BooleanField(default=False)
 
     # TODO link to consumption table if no value is given here?
-    consumption = models.FloatField(null=True)
-    length = models.FloatField()
+    consumption = models.FloatField(default=None, null=True)
+    length = models.FloatField(default=None, null = True)
 
 
 class Vehicle(models.Model):
     name = models.CharField(max_length=100, blank=False)
-    vehicle_type = models.ForeignKey(VehicleType, on_delete=models.CASCADE)
+    vehicle_type = models.ForeignKey(VehicleType, on_delete=models.CASCADE, null=True, blank=True)
     scenario = models.ForeignKey(Scenario, on_delete=models.CASCADE)
 
+    # ToDo insert output here or create other Class "VehicleOutput" which also contains the
+    # simulation results regarding this vehicle
+
+    # date_time_stamps = ArrayField(models.DateTimeField( default = None, null=True))
+    # socs = ArrayField(models.FloatField( default = None, null=True))
     def __str__(self):
         return self.name
 
@@ -151,8 +157,11 @@ class Trip(models.Model):
     arrival_stop = models.ForeignKey(BusStop, on_delete=models.CASCADE,  related_name="trip_arrival_set")
     arrival_time = models.DateTimeField(blank=False)
     distance = models.FloatField()
-    temperature = models.FloatField(default=None)
-    level_of_loading = models.FloatField(default=None)
+
+    # ToDo do we want a line object?
+    line = models.CharField(max_length=100, blank=True, null=True)
+    temperature = models.FloatField(default=None, null = True)
+    level_of_loading = models.FloatField(default=None, null = True)
     # If time resolution is minutes, there might be trips with 0 minutes duration. To resolve
     # division by 0, we use a minimal duration of 1 second
     @property
@@ -169,10 +178,23 @@ class Trip(models.Model):
 
 class Station(models.Model):
     # Map Engine models need geom and name as first columns
-    geom = models.PointField(srid=4326)  # with z elevation
+    geom = models.PointField(dim=3, srid=4326)  # with z elevation
     name = models.TextField()
 
     scenario = models.ForeignKey(Scenario, on_delete=models.CASCADE)
+
+    VOLTAGE_LEVEL_CHOICES = ["HV", "HV/MV", "MV", "MV/LV", "LV"]
+    CHARGE_TYPES = (("oppb", "Opportunity"), ("depb", "Depot"))
+
+    is_electrified = models.BooleanField(default=False)
+    charge_type = models.CharField(max_length=4, choices=CHARGE_TYPES, null=True)
+    voltage_level = models.CharField(max_length=5, choices=[(c, c) for c in VOLTAGE_LEVEL_CHOICES],
+                                     null=True)
+    amount_charging_places = models.IntegerField(default=0, null=True)
+    power_per_charger = models.FloatField(default = None, null = True)
+    total_power = models.FloatField(default = None, null = True)
+
+
 
     # prior attributes, used for map (?)
     objects = models.Manager()
@@ -207,18 +229,6 @@ class Station(models.Model):
         data["lat"] = obj.geom.x
         data["lon"] = obj.geom.y
         return data
-
-class ElectrifiedStation(Station):
-    VOLTAGE_LEVEL_CHOICES = ["HV", "HV/MV", "MV", "MV/LV", "LV"]
-    CHARGE_TYPES = (("oppb", "Opportunity"), ("depb", "Depot"))
-
-    charge_type = models.CharField(max_length=4, choices=CHARGE_TYPES, null=True)
-    voltage_level = models.CharField(max_length=5, choices=[(c, c) for c in VOLTAGE_LEVEL_CHOICES],
-                                     null=True)
-    amount_charging_places = models.IntegerField(default=0)
-    power_per_charger = models.FloatField(default = None)
-    total_power = models.FloatField()
-
 
 
 #
