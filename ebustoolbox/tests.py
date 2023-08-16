f31a1aba512c6239ec5c819a1faf4397c8ba7409
PaulScheer <paul.scheer@rl-institut.de>
2023-08-16 11:08:05 +0200

    Test scenario_creation

--- ebustoolbox/tests.py
+++ ebustoolbox/tests.py
@@ -1,2 +1,23 @@
+import time
+
+from django.test import TestCase
 # Create your tests here.
-a = 7
+from .models import Scenario
+
+class ScenarioTestCase(TestCase):
+    def setUp(self):
+        # Create test instances of your model
+        Scenario.objects.create(name='Instance 1')
+        time.sleep(0.01)
+        Scenario.objects.create(name='Instance 2')
+
+    def test_model_creation(self):
+        instance_1 = Scenario.objects.get(name='Instance 1')
+        instance_2 = Scenario.objects.get(name='Instance 2')
+        self.assertGreater(instance_2.created, instance_1.created)
+        self.assertIsNone(instance_1.finished)
+        self.assertIsInstance(instance_1.options, dict)
+        self.assertIsNone(instance_1.task_id)
+
+
+
