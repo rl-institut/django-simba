b3d458baa0490faa2f7d3612a0a11589b421d235
0pss <71946340+0pss@users.noreply.github.com>
2024-01-24 13:32:47 +0100

    fixed cut-off label in plotly-dash bug.

--- ebustoolbox/dash_app.py
+++ ebustoolbox/dash_app.py
@@ -9,14 +9,11 @@ from ebustoolbox.models import VehicleProperties, Vehicle, Scenario
 
 app = DjangoDash('MyDashApp')
 
-# Dummy function to fetch data based on user ID
 def fetch_data(user_id):
     # Replace this with your logic to fetch data based on user ID
     # For simplicity, just return some dummy data
-    print("fetching: " + user_id + " of type " + str(type(user_id)))
     scenario = Scenario.objects.get(task_id=user_id)
     vehicles = Vehicle.objects.filter(vehicle_type__scenario=scenario)
-    print(scenario.id, vehicles)
     vals = get_soc(scenario.id)
 
     data = {
@@ -30,52 +27,56 @@ def fetch_data(user_id):
 
     return data
 
-# Dummy function to create a Plotly figure
-def create_figure(data):
-    # Replace this with your logic to create a Plotly figure
-    # For simplicity, just create a scatter plot
-    print(data)
-
+def create_line_chart(data):
+    # Create a line chart using Plotly
     u_id = data["user_id"]
     data.pop("user_id")
-
-
-
-    # Use px.line to plot multiple lines dynamically
-    #fig = px.line(df, x=df.index + 1, y=df.columns[1:], title=f'Data for User {data["user_id"]}')
     dict_of_fig = dict({
-        "layout": {"legend": {"orientation": "h", "itemsizing": "constant", "itemwidth": 300, "tracegroupgap" : 50}}
+        "layout": {"legend": {"orientation": "v", "itemsizing": "constant", "itemwidth": 50, "tracegroupgap": 50}}
     })
-
     fig = go.Figure(dict_of_fig)
     for v_name, y_values in data.items():
         x_values = list(range(1, len(y_values) + 1))
-        fig.add_trace(go.Scatter(x=x_values, y=y_values, name=v_name, line=dict(width=4)))
+        fig.add_trace(go.Line(x=x_values, y=y_values, name=v_name, line=dict(width=4)))
 
-    #fig.update_layout(title={"font_size": 22, "xanchor": "center", "x": 0.5})
     fig.update_layout(title=f'Data for User {u_id}')
-    #fig.update_layout(legend=dict(font=dict(family="Courier", size=50, color="black")),
-     #                 legend_title=dict(font=dict(family="Courier", size=30, color="blue")))
 
     return fig
 
+def create_pie_chart(data):
+    # Create a pie chart using Plotly Express
+    fig = px.pie(names=list(data.keys()), values=list(map(len, data.values())), title=f'Pie Chart for User {data["user_id"]}')
+    return fig
+
+# Layout of the app
 app.layout = html.Div([
     dcc.Input(id='userid', type='hidden', value=''),
-    dcc.Graph(id='my-graph', config={'displayModeBar': False}, style={'height': '700px'}),
-],style={'width': '100%', 'overflow': 'visible', 'height': '700px'})
+    dcc.Graph(id='line-chart', config={'displayModeBar': False}, style={'height': '550px'}),
+    dcc.Graph(id='pie-chart', config={'displayModeBar': False}, style={'height': '550px'}),
+], style={'width': '100%', 'overflow': 'visible', 'height': '700px'})
 
+# Callback to update the line chart based on user input
 @app.callback(
-    Output('my-graph', 'figure'),
+    Output('line-chart', 'figure'),
     [Input('userid', 'value')]
 )
-def update_graph(value):
+def update_line_chart(value):
     # Fetch data based on the user ID
-    print()
-    print('IDDDDD: ' + str(value))
     data = fetch_data(value)
-    print("Now new plot???")
 
-    # Create and return the Plotly figure
-    figure = create_figure(data)
+    # Create and return the line chart
+    figure = create_line_chart(data)
     return figure
 
+# Callback to update the pie chart based on user input
+@app.callback(
+    Output('pie-chart', 'figure'),
+    [Input('userid', 'value')]
+)
+def update_pie_chart(value):
+    # Fetch data based on the user ID
+    data = fetch_data(value)
+
+    # Create and return the pie chart
+    figure = create_pie_chart(data)
+    return figure
