2224578ad407e31dae1127c69eea0dc74dfb5014
PaulScheer <paul.scheer@rl-institut.de>
2024-02-26 16:40:55 +0100

    Add memoization decorator

--- dash_app/data.py
+++ dash_app/data.py
@@ -6,10 +6,8 @@ import numpy as np
 
 from ebustoolbox.models import (
     Scenario,
-    Vehicle,
     Event,
     Route,
-    Trip,
     get_longest_distance_rotation,
     get_shortest_distance_rotation,
     EventType,
@@ -17,22 +15,27 @@ from ebustoolbox.models import (
     VehicleType,
 )
 import pandas as pd
-from django.db.models import Min, Sum, Count
+from django.db.models import Min, Count
 from dash.exceptions import PreventUpdate
 
 
-import time
-df_perf = pd.DataFrame({'name': [], 'start': [], 'end': [], 'process': []})
+df_perf = pd.DataFrame({"name": [], "start": [], "end": [], "process": []})
+
 
 def get_all_buses(task_id: str) -> list[str]:
     s = Scenario.objects.get(task_id=task_id)
     all_buses = list(Vehicle.objects.filter(scenario=s).values_list("name_short", flat=True))
     return all_buses
 
+
 def get_number_of_buses(filter_dict: dict) -> list[str]:
     task_id = filter_dict.pop("task_id")
     vehicles = filter_dict.pop("vehicle__name_short__in")
-    return ["Selected / Total number of Buses:", str(len(vehicles)) + " / " + str(len(get_all_buses(task_id)))]
+    return [
+        "Selected / Total number of Buses:",
+        str(len(vehicles)) + " / " + str(len(get_all_buses(task_id))),
+    ]
+
 
 def get_number_longest_rot(filter_dict: dict):
 
@@ -47,6 +50,7 @@ def get_number_longest_rot(filter_dict: dict):
 
     return [f"Longest Rotation {longest_rotation.name}", f"{longest_rotation.distance} m"]
 
+
 def get_number_shortest_rot(filter_dict: dict):
 
     task_id = filter_dict.pop("task_id")
@@ -60,6 +64,7 @@ def get_number_shortest_rot(filter_dict: dict):
 
     # Add style if text should have special style
     return [f"Shortest Rotation {shortest_rotation.name}", f"{shortest_rotation.distance} m"]
+
 
 def get_scatter_plot_data(filter_dict: dict) -> dict:
 
@@ -86,6 +91,7 @@ def get_scatter_plot_data(filter_dict: dict) -> dict:
         data[vehicle.name_short] = [times, socs]
 
     return data
+
 
 def get_bar_plot_data(filter_dict: dict) -> list[str, float, float]:
 
@@ -121,6 +127,7 @@ def get_bar_plot_data(filter_dict: dict) -> list[str, float, float]:
 
     return data
 
+
 def get_soc(scenario_id):
     """
     Get SoC timeseries for each vehicle in a scenario
@@ -138,50 +145,107 @@ def get_soc(scenario_id):
     for event in events:
         if event.vehicle_id not in socs:
             socs[event.vehicle_id] = []
-        socs[event.vehicle_id].append({
-            "station":    event.station_id,
-            "time_start": event.time_start.isoformat(),
-            "time_end": event.time_end.isoformat(),
-            "soc_start":  event.soc_start,
-            "soc_end":    event.soc_end,
-            "timeseries": event.timeseries["soc"],
-        })
+        socs[event.vehicle_id].append(
+            {
+                "station": event.station_id,
+                "time_start": event.time_start.isoformat(),
+                "time_end": event.time_end.isoformat(),
+                "soc_start": event.soc_start,
+                "soc_end": event.soc_end,
+                "timeseries": event.timeseries["soc"],
+            }
+        )
 
     return socs
 
-def get_soc_as_dataframe(scenario_id, buses):
+
+def recent_memoizer(function, _dcache1=dict(), _result_cache2=dict()):
+    """Decorator function
+
+    :param function: function do be decorated
+    :type function: function
+    :param _dcache1: storage of recent calls
+    :param _result_cache2: storage of results of recent calls
+    :return: decorated function or timer if given function is None
+    :rtype function or dict
+
+    """
+    # Maximum number of cached results per function
+    MAX_SIZE = 10
+
+    def decorated_function(*this_args, **kwargs):
+        key = function.__name__
+        if key not in _dcache1:
+            _dcache1[key] = list()
+            _result_cache2[key] = dict()
+
+        inputs = tuple((this_args, *list(kwargs)))
+        if inputs in _dcache1[key] and inputs in _result_cache2[key]:
+            _dcache1[key].remove(inputs)
+            _dcache1[key].append(inputs)
+            return _result_cache2[key][inputs]
+
+        else:
+            # Storage is full. Delete oldest storage
+            if len(_dcache1[key]) >= MAX_SIZE:
+                del _result_cache2[key][_dcache1[key][0]]
+                del _dcache1[key][0]
+            _dcache1[key].append(inputs)
+
+        return_value = function(*this_args, **kwargs)
+        _result_cache2[key][inputs] = return_value
+        return return_value
+
+    return decorated_function
+
+
+@recent_memoizer
+def get_all_soc_as_dataframe(scenario_id):
+    # if scenario_id in _cache:
+    #     _cache.remove(scenario_id)
+    #     _cache.append(scenario_id)
+    #     return _dcache[scenario_id]
+    # else:
+    #     if len(_cache) >= 10:
+    #         del _dcache[_cache[0]]
+    #         del _cache[0]
+    #     _cache.append(scenario_id)
 
     vehicles = Vehicle.objects.filter(scenario_id=scenario_id)
     scenario = Scenario.objects.get(id=scenario_id)
     # get all vehicle events from this scenario at a station
 
-
     dfs = []
 
     for vehicle in vehicles:
-        if vehicle.name_short in buses:
-            v_id = vehicle.id
-            events = scenario.event_set.filter(vehicle__isnull=False, vehicle_id=v_id)
-            for event in events:
-                time_start = event.time_start
-                soc_start = event.soc_start
-                # Add a row to the DataFrame
-                df = pd.DataFrame({'V_id': [v_id], 'Time': [time_start], 'SOC': [soc_start]})
-                dfs.append(df)
-                time_end = event.time_end
-                soc_end = event.soc_end
-                # Add a row to the DataFrame
-                df = pd.DataFrame({'V_id': [v_id], 'Time': [time_end], 'SOC': [soc_end]})
-                dfs.append(df)
+        v_id = vehicle.name_short
+        events = scenario.event_set.filter(vehicle__isnull=False, vehicle_id=vehicle.id)
+        for event in events:
+            time_start = event.time_start
+            soc_start = event.soc_start
+            # Add a row to the DataFrame
+            df = pd.DataFrame({"V_id": [v_id], "Time": [time_start], "SOC": [soc_start]})
+            dfs.append(df)
+            time_end = event.time_end
+            soc_end = event.soc_end
+            # Add a row to the DataFrame
+            df = pd.DataFrame({"V_id": [v_id], "Time": [time_end], "SOC": [soc_end]})
+            dfs.append(df)
     result_df = pd.concat(dfs, ignore_index=True)
 
     # Convert the 'Time' column to datetime format
-    result_df['Time'] = pd.to_datetime(result_df['Time'])
+    result_df["Time"] = pd.to_datetime(result_df["Time"])
 
     # Sort the DataFrame based on the 'Time' column
-    result_df = result_df.sort_values(by='Time')
+    result_df = result_df.sort_values(by="Time")
 
+    # _dcache[scenario_id] = result_df
     return result_df
+
+
+def get_soc_as_dataframe(scenario_id, buses):
+    result_df = get_all_soc_as_dataframe(scenario_id)
+    return result_df.query(f"V_id in {buses}")
 
 
 def get_activities_as_dataframe(scenario_id, buses):
@@ -200,18 +264,28 @@ def get_activities_as_dataframe(scenario_id, buses):
             for event in events:
                 time_start = event.time_start
                 event_type = event.event_type
-                duration = (pd.to_datetime(event.time_end) - pd.to_datetime(event.time_start)).total_seconds()
+                duration = (
+                    pd.to_datetime(event.time_end) - pd.to_datetime(event.time_start)
+                ).total_seconds()
                 time_end = event.time_end
                 # Add a row to the DataFrame
-                df = pd.DataFrame({'V_id': [v_id], 'time_start': [time_start],'time_end': [time_end],'duration': [duration], 'event_type': [event_type]})
+                df = pd.DataFrame(
+                    {
+                        "V_id": [v_id],
+                        "time_start": [time_start],
+                        "time_end": [time_end],
+                        "duration": [duration],
+                        "event_type": [event_type],
+                    }
+                )
                 dfs.append(df)
     result_df = pd.concat(dfs, ignore_index=True)
 
     # Convert the 'Time' column to datetime format
-    result_df['time_start'] = pd.to_datetime(result_df['time_start'])
-    result_df['time_end'] = pd.to_datetime(result_df['time_end'])
+    result_df["time_start"] = pd.to_datetime(result_df["time_start"])
+    result_df["time_end"] = pd.to_datetime(result_df["time_end"])
     # Sort the DataFrame based on the 'Time' column
-    result_df = result_df.sort_values(by='time_start')
+    result_df = result_df.sort_values(by="time_start")
 
     return result_df
 
@@ -236,16 +310,19 @@ def get_distances_as_dataframe(scenario_id, buses):
                     for route in routes:
                         distance = route.distance
 
-                        df = pd.DataFrame({'V_id': [v_id], 'R_id': [r_id], 'total_distance': [distance]})
+                        df = pd.DataFrame(
+                            {"V_id": [v_id], "R_id": [r_id], "total_distance": [distance]}
+                        )
                         dfs.append(df)
 
     result_df = pd.concat(dfs, ignore_index=True)
-    result_df = result_df.groupby('R_id')['total_distance'].sum().reset_index()
+    result_df = result_df.groupby("R_id")["total_distance"].sum().reset_index()
 
     return result_df
 
 
-def get_duration_as_dataframe(scenario_id, buses):
+@recent_memoizer
+def get_all_durations_as_dataframe(scenario_id):
     vehicles = Vehicle.objects.filter(scenario_id=scenario_id)
     scenario = Scenario.objects.get(id=scenario_id)
     # get all vehicle events from this scenario at a station
@@ -253,20 +330,29 @@ def get_duration_as_dataframe(scenario_id, buses):
     dfs = []
 
     for vehicle in vehicles:
-        if vehicle.name_short in buses:
-            v_id = vehicle.id
-            rotations = scenario.rotation_set.filter(vehicle__isnull=False, vehicle_id=v_id)
-            for rotation in rotations:
-                r_id = rotation.id
-                trips = scenario.trip_set.filter(rotation_id=rotation.id)
-                for trip in trips:
-                    duration = (pd.to_datetime(trip.arrival_time) - pd.to_datetime(trip.departure_time)).total_seconds()
-                    df = pd.DataFrame({'V_id': [v_id], 'R_id': [r_id], 'duration': [duration]})
-                    dfs.append(df)
+        v_id = vehicle.id
+        rotations = scenario.rotation_set.filter(vehicle__isnull=False, vehicle_id=v_id)
+        for rotation in rotations:
+            r_id = rotation.id
+            trips = scenario.trip_set.filter(rotation_id=rotation.id)
+            for trip in trips:
+                duration = (
+                    pd.to_datetime(trip.arrival_time) - pd.to_datetime(trip.departure_time)
+                ).total_seconds()
+                df = pd.DataFrame(
+                    {"V_id": [vehicle.name_short], "R_id": [r_id], "duration": [duration]}
+                )
+                dfs.append(df)
     result_df = pd.concat(dfs, ignore_index=True)
-    result_df = result_df.groupby('R_id')['duration'].sum().reset_index()
+    result_df = result_df.groupby(["R_id", "V_id"])["duration"].sum().reset_index()
 
     return result_df
+
+
+def get_duration_as_dataframe(scenario_id, buses):
+    result_df = get_all_durations_as_dataframe(scenario_id)
+    return result_df.query(f"V_id in {buses}")
+
 
 def get_powerdraw_as_dataframe(scenario_id, buses):
 
@@ -280,36 +366,51 @@ def get_powerdraw_as_dataframe(scenario_id, buses):
         if vehicle.name_short in buses:
             v_id = vehicle.id
             v_typeid = vehicle.vehicle_type_id
-            batterycapacity = VehicleType.objects.get(id=v_typeid)#
-            charge_eff = VehicleType.objects.get(id=v_typeid)#
+            batterycapacity = VehicleType.objects.get(id=v_typeid)  #
+            charge_eff = VehicleType.objects.get(id=v_typeid)  #
 
-            events = scenario.event_set.filter(vehicle__isnull=False, station_id__isnull=False, vehicle_id=v_id)
+            events = scenario.event_set.filter(
+                vehicle__isnull=False, station_id__isnull=False, vehicle_id=v_id
+            )
             for event in events:
-                time_start = event.time_start
                 soc_start = event.soc_start
                 station = event.station_id
                 # Add a row to the DataFrame
                 time_end = event.time_end
                 soc_end = event.soc_end
                 if soc_end > soc_start:
-                    energy = soc_end-soc_start * charge_eff * batterycapacity
+                    energy = soc_end - soc_start * charge_eff * batterycapacity
                     # Add a row to the DataFrame
-                    df = pd.DataFrame({'V_id': [v_id], 'Time': [time_end], 'Energy': [energy], 'Station_id': [station] })
+                    df = pd.DataFrame(
+                        {
+                            "V_id": [v_id],
+                            "Time": [time_end],
+                            "Energy": [energy],
+                            "Station_id": [station],
+                        }
+                    )
                     dfs.append(df)
-    if dfs: #if not empty
+    if dfs:  # if not empty
         result_df = pd.concat(dfs, ignore_index=True)
     else:
-        result_df = pd.DataFrame({'V_id': [None], 'Time': [None], 'Energy': [None], 'Station_id': [None]})
-
+        result_df = pd.DataFrame(
+            {"V_id": [None], "Time": [None], "Energy": [None], "Station_id": [None]}
+        )
 
     return result_df
+
 
 def get_vehicle_types(scenario_id, buses):
     filter_dict = dict(scenario_id=scenario_id)
 
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!111",len(VehicleType.objects.filter(**filter_dict)))
+    print(
+        "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!111",
+        len(VehicleType.objects.filter(**filter_dict)),
+    )
 
-    values_with_counts = VehicleType.objects.filter(**filter_dict).values('name').annotate(count=Count('name'))
+    values_with_counts = (
+        VehicleType.objects.filter(**filter_dict).values("name").annotate(count=Count("name"))
+    )
 
     print("$$$$$$$$$$$$$$$$$$$$$$444")
     print(values_with_counts)
@@ -318,19 +419,29 @@ def get_vehicle_types(scenario_id, buses):
 
     return df
 
+
 def get_df_perf():
     global df_perf
     return df_perf
 
+
 def register_time(name, start, end, process):
     global df_perf
-    df = pd.DataFrame({'name': [name], 'start': [pd.to_datetime(start, unit='s')], 'end': [pd.to_datetime(end, unit='s')], 'process': [process]})
-    #df['start'] = pd.to_datetime(df['start'])
-    #df['end'] = pd.to_datetime(df['end'])
+    df = pd.DataFrame(
+        {
+            "name": [name],
+            "start": [pd.to_datetime(start, unit="s")],
+            "end": [pd.to_datetime(end, unit="s")],
+            "process": [process],
+        }
+    )
+    # df['start'] = pd.to_datetime(df['start'])
+    # df['end'] = pd.to_datetime(df['end'])
     df_perf = pd.concat([df_perf, df], ignore_index=True, sort=False)
+
 
 def reset_df_perf():
     global df_perf
-    df_perf = pd.DataFrame({'name': [], 'start': [], 'end': [], 'process': []})
-    df_perf['start'] = pd.to_datetime(df_perf['start'])
-    df_perf['end'] = pd.to_datetime(df_perf['end'])
+    df_perf = pd.DataFrame({"name": [], "start": [], "end": [], "process": []})
+    df_perf["start"] = pd.to_datetime(df_perf["start"])
+    df_perf["end"] = pd.to_datetime(df_perf["end"])
