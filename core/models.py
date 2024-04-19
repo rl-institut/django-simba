974ae9aa82351ea15b7e9fba59d8624ffaa83719
PaulScheer <paul.scheer@rl-institut.de>
2024-04-19 10:32:19 +0200

    Add convenience methods for Progress

--- core/models.py
+++ core/models.py
@@ -20,3 +20,24 @@ class Progress(models.Model):
             return self.current_work / self.total_work * 100
         except ZeroDivisionError:
             return 0
+
+    def set_success(self):
+        self.status = "Finished"
+        self.current_work = self.total_work
+        self.success = True
+        self.running = False
+        self.save()
+
+    def set_failed(self):
+        self.status = "Failed"
+        self.success = False
+        self.running = False
+        self.save()
+
+    def reset(self):
+        self.status = "Started"
+        self.current_work = 0
+        self.total_work = 1
+        self.success = False
+        self.running = True
+        self.save()
