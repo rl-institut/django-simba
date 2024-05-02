2f88332c0cbcce87fb7cb325c3bf206c2bfe36c3
0pss <71946340+0pss@users.noreply.github.com>
2024-05-02 15:25:08 +0200

    still testing the if this beaks the tests#

--- eflips_depot_results/__init__.py
+++ eflips_depot_results/__init__.py
@@ -64,9 +64,9 @@ def _create_engine_from_postgis_url() -> sqlalchemy.engine.Engine:
 
     print("The Problem is here: "+ os.environ.get("DATABASE_URL").replace("postgis", "postgresql"))
     print("instead use:")
-    print("postgresql://postgres:postgres@localhost/default")
+    print("postgresql://myprojectuser:1234@localhost/mydb")
 
-    return sqlalchemy.create_engine("postgresql://postgres:postgres@localhost/default")
+    return sqlalchemy.create_engine("postgresql://myprojectuser:1234@localhost/mydb")
 
 
 @app.callback(
