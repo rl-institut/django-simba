cc134952d6336242d6ca36cf9f0397053eae7b56
PaulScheer <paul.scheer@rl-institut.de>
2024-05-22 16:32:18 +0200

    Add elevation_api misc

---
+++ elevation_api/urls.py
@@ -0,0 +1,20 @@
+"""URLs for map app, including main view and API points."""
+
+from django.urls import path
+
+from . import views
+
+app_name = "elevation_api"
+
+urlpatterns = [
+    path(
+        "api/v1/lookup",
+        views.elevation_view,
+        name="elevation_view",
+    ),
+    path(
+        "<str:lat_long_query>",
+        views.elevation_view,
+        name="elevation_view",
+    ),
+]
