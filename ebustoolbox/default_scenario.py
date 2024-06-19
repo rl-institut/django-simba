c762191fa295d992deeac5eef293ecb7b357fa52
mosc5 <moritz.schiel@rl-institut.de>
2024-06-19 09:24:15 +0200

    rework default bus types

--- ebustoolbox/default_scenario.py
+++ ebustoolbox/default_scenario.py
@@ -31,45 +31,31 @@ def set_default_scenario(apps, schema_editor):
 
 
 def set_default_vehicle_types(apps, scenario) -> None:
-    battery_capacity = 150
+    vehicle_data_list = [
+        ("mini_bus", 120, 7, 0.9),
+        ("mini_bus_add_heating", 120, 7, 0.7),
+        ("midi_bus", 325, 10, 1.8),
+        ("midi_bus_add_heating", 450, 10, 1.3),
+        ("solo_bus", 400, 12, 2),
+        ("solo_bus_add_heating", 500, 12, 1.4),
+        ("articulated_bus", 500, 18, 2.6),
+        ("articulated_bus_add_heating", 650, 18, 1.8),
+    ]
+    for vehicle_data in vehicle_data_list:
+        set_default_vehicle_type(apps, scenario, *vehicle_data)
+
+
+def set_default_vehicle_type(apps, scenario, name, battery_capacity, length, consumption) -> None:
     vehicle_type_args = {
-        "name": "10m_bus",
+        "name": name,
         "scenario": scenario,
         "battery_capacity": battery_capacity,
         "charging_curve": [[0, battery_capacity], [1, battery_capacity]],
-        "length": 10,
-        "empty_mass": 13_000 * 10 / 12,
-        "allowed_mass": 19_000 * 10 / 12,
-        "consumption": 1.0,
+        "length": length,
+        "empty_mass": 13_000 * length / 12,
+        "allowed_mass": 19_000 * length / 12,
+        "consumption": consumption,
     }
     VehicleType = apps.get_model("ebustoolbox", "VehicleType")
-    _ = VehicleType.objects.create(**vehicle_type_args, opportunity_charging_capable=True)
-    _ = VehicleType.objects.create(**vehicle_type_args, opportunity_charging_capable=False)
-
-    battery_capacity = 200
-    vehicle_type_args = {
-        "name": "12m_bus",
-        "scenario": scenario,
-        "battery_capacity": battery_capacity,
-        "charging_curve": [[0, battery_capacity], [1, battery_capacity]],
-        "length": 12,
-        "empty_mass": 13_000,
-        "allowed_mass": 19_000,
-        "consumption": 1.2,
-    }
-    _ = VehicleType.objects.create(**vehicle_type_args, opportunity_charging_capable=True)
-    _ = VehicleType.objects.create(**vehicle_type_args, opportunity_charging_capable=False)
-
-    battery_capacity = 100
-    vehicle_type_args = {
-        "name": "18m_bus",
-        "scenario": scenario,
-        "battery_capacity": battery_capacity,
-        "charging_curve": [[0, battery_capacity], [1, battery_capacity]],
-        "length": 18,
-        "empty_mass": 13_000 * 1.5,
-        "allowed_mass": 19_000 * 1.5,
-        "consumption": 2.0,
-    }
     _ = VehicleType.objects.create(**vehicle_type_args, opportunity_charging_capable=True)
     _ = VehicleType.objects.create(**vehicle_type_args, opportunity_charging_capable=False)
