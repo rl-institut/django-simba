751b84302566715a3aa083a6e872089228b52b2d
0pss <71946340+0pss@users.noreply.github.com>
2024-06-26 15:13:16 +0200

    Fixed styling for some plots

--- dash_app/style.py
+++ dash_app/style.py
@@ -1,29 +1,39 @@
 from dash import dash
 import plotly.graph_objs as go
+from dash import dcc, html
 
 
 def set_styling(func):
     def wrapper(*args, **kwargs):
-
         font_family = "Helvetica"
         title_font_size = 25
         axis_font_size = 15
         hover_font_size = 20
-        print("HERER")
 
-        fig = func(*args, **kwargs)
-        print(type(fig))
-        # Check if fig is of type plotly.graph_objs.Figure
-        if not isinstance(fig, go.Figure):
+        result = func(*args, **kwargs)
+
+        def apply_styling(fig):
+            fig.update_layout(
+                font_family=font_family,
+                font=dict(size=axis_font_size),
+                title_font=dict(family=font_family, size=title_font_size),
+                xaxis_title_font=dict(family=font_family, size=axis_font_size),
+                yaxis_title_font=dict(family=font_family, size=axis_font_size),
+                hoverlabel=dict(font=dict(family=font_family, size=hover_font_size))
+            )
             return fig
 
-        fig.update_layout(
-            font_family=font_family,
-            font=dict(size=axis_font_size),
-            title_font=dict(family=font_family, size=title_font_size),
-            xaxis_title_font=dict(family=font_family, size=axis_font_size),
-            yaxis_title_font=dict(family=font_family, size=axis_font_size),
-            hoverlabel=dict(font=dict(family=font_family, size=hover_font_size))
-        )
-        return fig
+        if isinstance(result, html.Div):
+            child = result.children
+            if isinstance(child, dcc.Graph) and isinstance(child.figure, go.Figure):
+                child.figure = apply_styling(child.figure)
+            else:
+                print("Non stylable Object")
+            return result
+
+        if isinstance(result, go.Figure):
+            return apply_styling(result)
+
+        return result
+
     return wrapper
