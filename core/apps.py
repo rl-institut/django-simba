934bb8007d9c82eb9051a89bb278a8116cd5a32a
PaulScheer <paul.scheer@rl-institut.de>
2024-11-08 09:40:23 +0100

    Add database info at Server Startup

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
+
+    def ready(self):
+        from django.db import connection
+        from django.conf import settings
+
+        with connection.cursor() as cursor:
+            db_name = settings.DATABASES["default"]["NAME"]
+            cursor.execute(f"SELECT pg_size_pretty(pg_database_size('{db_name}'))")
+            print(f"Default DB {db_name} has size of {cursor.fetchone()[0]}.")
