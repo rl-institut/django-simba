81ec3364a84f0e1f280ffbf855d7c257ec31c87d
PaulScheer <paul.scheer@rl-institut.de>
2025-01-13 15:04:57 +0100

    Add filter for some messages with status_code 200 or 204

--- ebusdjango/settings.py
+++ ebusdjango/settings.py
@@ -77,7 +77,6 @@ INSTALLED_APPS = [
     "bootstrap4",
 ]
 
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -144,7 +143,6 @@ if CELERY_USE:
         "used, this URL has to be provided"
     )
 
-
 # For Database visualization
 GRAPH_MODELS = {
     "all_applications": False,
@@ -170,7 +168,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
@@ -184,9 +181,32 @@ LOGGING = {
             "style": "{",
         },
     },
+    "filters": {
+        "plotly_dash_status_ok": {
+            "()": "core.filters.FilterStatusCode",
+            "status_code": 200,
+            "search_text": "_dash-update-component",
+        },
+        "plotly_dash_status_no_content": {
+            "()": "core.filters.FilterStatusCode",
+            "status_code": 204,
+            "search_text": "_dash-update-component",
+        },
+        "map_status_no_content": {
+            "()": "core.filters.FilterStatusCode",
+            "status_code": 204,
+            "search_text": "/map/stations_mvt_mvt/",
+        },
+    },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
+            # These logs clutter the console and are not very helpful
+            "filters": [
+                "map_status_no_content",
+                "plotly_dash_status_ok",
+                "plotly_dash_status_no_content",
+            ],
             "formatter": "simple",
         },
         "file": {
@@ -213,7 +233,6 @@ LOGGING = {
         },
     },
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
