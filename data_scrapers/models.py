6d2a6924da29268d956200138877ac2bfb93dcc6
PaulScheer <paul.scheer@rl-institut.de>
2025-05-07 15:49:20 +0200

    Add docstrings for models

--- data_scrapers/models.py
+++ data_scrapers/models.py
@@ -1,5 +1,4 @@
 import logging
-
 from django.contrib.gis.db import models
 
 logger = logging.getLogger("custom")
@@ -7,6 +6,18 @@ logger = logging.getLogger("custom")
 
 # https://wiki.openstreetmap.org/wiki/Overpass_API/Overpass_QL#By_element_id
 class AdminArea(models.Model):
+    """
+    Model for hierarchical structuring of AdminAreas.
+
+    Attributes:
+        name (str): Name of the AdminArea
+        admin_level (int): Administrative level of the AdminArea
+        osm_id (int): OpenStreetMap identifier
+        updated_at (Datetime): Last time this was updated
+        upper_admin_area (int|AdminArea|None): AdminArea with the highest admin_level
+        the current AdminArea is contained in.
+    """
+
     name = models.CharField(max_length=100)
     admin_level = models.IntegerField(default=4)
     osm_id = models.BigIntegerField(unique=True)
@@ -15,6 +26,16 @@ class AdminArea(models.Model):
 
 
 class BusStation(models.Model):
+    """
+    Model for spatial resolving of BusStations
+
+    Attributes:
+        name (str): Name of BusStation
+        osm_id (int): OpenStreetMap identifier
+        geom (django.contrib.gis.geos.Point|None)): Geolocation of BusStation in SRID 4326
+        admin_area (int|AdminArea): AdminArea with the highest admin_level the BusStation is contained in.
+    """
+
     name = models.CharField(max_length=100)
     osm_id = models.BigIntegerField(unique=True)
     geom = models.PointField(dim=3, srid=4326, null=True)
