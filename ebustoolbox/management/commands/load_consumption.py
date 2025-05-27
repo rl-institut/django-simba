82904e20560f716585f43cd9bddc5e016a38c64c
PaulScheer <paul.scheer@rl-institut.de>
2025-05-27 11:29:18 +0200

    Link simulated and emperical consumptions with default vehicle types

--- ebustoolbox/management/commands/load_consumption.py
+++ ebustoolbox/management/commands/load_consumption.py
@@ -1,15 +1,21 @@
+import json
+import logging
 from django.core.management.base import BaseCommand
+from matplotlib import pyplot as plt
 from ebustoolbox.default_scenario import get_default_scenario
 from ebustoolbox.models import Consumption, VehicleType, VehicleClass, Scenario, DefaultScenario
 import pandas as pd
 from pathlib import Path
+from ebustoolbox.util import generate_consumption_lut_plot
+
+logger = logging.getLogger("custom")
 
 
 class Command(BaseCommand):
     help = "Load Consumption tables and connect them with default Vehicle Types"
 
     def handle(self, *args, **kwargs):
-        scenario = get_default_scenario(DefaultScenario, Scenario).scenario
+        # Set the default vehicle types
         root = "./ebustoolbox/static/ebustoolbox/examples/"
         consumption_paths = [
             (10, root + "consumption_ebus2030_no_diesel_10m.csv"),
@@ -24,6 +30,7 @@ class Command(BaseCommand):
             (7, root + "consumption_ebus2030_w_diesel_7m.csv"),
         ]
 
+        scenario = get_default_scenario(DefaultScenario, Scenario).scenario
         default_vts = VehicleType.objects.filter(scenario=scenario)
         for length, path in consumption_paths:
             vts = default_vts.filter(length=length)
@@ -55,3 +62,54 @@ class Command(BaseCommand):
                 consumption.scenario = scenario
                 consumption.vehicle_class = vehicle_class
                 consumption.save()
+                figure = generate_consumption_lut_plot(consumption)
+                figure.savefig("consumption_" + vt.name + ".pdf")
+                plt.close()
+        df = pd.read_csv(root + "tu_bvg_lut.csv")
+        # Read TU Berlin bvg data
+        logger.warning("Emperical data is incomplete. GN Data is used for [EN,DD,GN] vehicle types")
+        for name in ["EN", "DD", "GN"]:
+            vts = default_vts.filter(name=name)
+            for _, row in df.iterrows():
+                # TODO: only GN has complete datapoints. other data_points for other vehicle types
+                # dont end syntactically correct, e.g. with "...{10,10" without closing brackets
+
+                # The commented code is what should be done if we had complete data
+                # if name.lower() not in row["name"].lower():
+                # instead
+                if "gn" not in row["name"].lower():
+                    continue
+                columns = json.loads(row["columns"].replace("{", "[").replace("}", "]"))
+                data_points = json.loads(row["data_points"].replace("{", "[").replace("}", "]"))
+                values = json.loads(row["values"].replace("{", "[").replace("}", "]"))
+                for vt in vts:
+                    vehicle_class = VehicleClass.objects.filter(
+                        vehicle_types=vt,
+                    )
+                    if vehicle_class.exists():
+                        assert vehicle_class.count() == 1
+                        vehicle_class = vehicle_class.first()
+                    else:
+                        vehicle_class = VehicleClass(
+                            scenario=scenario,
+                            name=f"Consumption Vehicle Class for default vehicle {vt.name} {vt.id}",
+                        )
+                        vehicle_class.save()
+                        vehicle_class.vehicle_types.add(vt)
+                    # Delete old consumptions which might point to the default vehicles
+                    Consumption.objects.filter(vehicle_class=vehicle_class).delete()
+                    consumption = Consumption(
+                        name=f"Default Emperical Consumption for default vehicle {vt.name} with id {vt.id}",
+                        vehicle_class=vehicle_class,
+                        scenario=scenario,
+                        columns=columns,
+                        data_points=data_points,
+                        values=values,
+                    )
+                    consumption.save()
+                    figure = generate_consumption_lut_plot(consumption)
+                    figure.savefig("consumption_" + vt.name + ".pdf")
+                    plt.close()
+                break
+            else:
+                print(name, " not found")
--- ebustoolbox/util.py
+++ ebustoolbox/util.py
@@ -1,14 +1,15 @@
 import base64
-
-import django
 from celery import uuid
 from io import BytesIO
 import matplotlib
+from matplotlib import pyplot as plt
+import pandas as pd
 import sys
 
+import django
 from django.db.models import Max
 
-from .models import Scenario
+from .models import Consumption, Scenario
 from dash_app.data import get_powerdraw_as_dataframe
 
 if not any(["selenium" in str(x) for x in sys.modules.values()]):
@@ -57,3 +58,81 @@ def get_next_id(manager: django.db.models.Manager) -> int:
     if manager.objects.exists():
         return manager.objects.aggregate(Max("id"))["id__max"] + 1
     return 1
+
+
+def generate_consumption_lut_plot(consumption: Consumption):
+    X_AXIS = "mean_speed_kmh"
+    Y_AXIS = "consumption_kwh_per_km"
+    CONSTANT_VALUES = [
+        ("level_of_loading", 0.5),
+        ("incline", 0.0),
+    ]
+    df = consumption.to_df()
+    for column in [X_AXIS, Y_AXIS, *[t[0] for t in CONSTANT_VALUES]]:
+        assert column in df.columns, f"Column {column} is missing in dataframe"
+
+    df_reduced = df.copy()
+    for column, value in CONSTANT_VALUES:
+        mask = df_reduced.loc[:, column] == value
+        df_reduced = df_reduced.loc[mask, :]
+        if not any(mask):
+            print(f"Column {column} with value {value} not found")
+            return
+    df = df_reduced
+    figure, ax = generate_2d_plot_from_lut(df_reduced, x_axis=X_AXIS, y_axis=Y_AXIS)
+    ax.set_xlabel("Speed [km/h]")
+    ax.set_ylabel("Consumption [kWh/km]")
+    title = str(consumption.name)
+    second_half = title[len(title) // 2 :]
+    i = second_half.find(" ")
+    linebreak = len(title) // 2 + i
+    title = title[:linebreak] + "\n" + title[linebreak:]
+    ax.set_title(title)
+    ax.legend(loc="upper right")
+    descr = "\n".join((("=".join(str(x) for x in t) for t in CONSTANT_VALUES)))
+    ax.annotate(
+        descr,
+        (1, 0.5),
+        ha="right",
+        xycoords="axes fraction",
+        xytext=(0, 0),
+        textcoords="offset points",
+    )
+    return figure
+
+
+def generate_2d_plot_from_lut(df: pd.DataFrame, x_axis: str, y_axis: str):
+    x_axis = "mean_speed_kmh"
+    y_axis = "consumption_kwh_per_km"
+    # Get remaining variations
+    variation_column = None
+    for column in df.columns:
+        if column in [x_axis, y_axis]:
+            continue
+        if len(df.loc[:, column].unique()) > 1:
+            assert (
+                variation_column is None
+            ), f"Plot cannot have two variation columns {variation_column} and {column}"
+            variation_column = column
+
+    for x_value in df.loc[:, x_axis].unique():
+        mask = df.loc[:, x_axis] == x_value
+        # Make sure that the variation column is unique per x_axis value.
+        assert len(df.loc[mask, variation_column].unique()) == len(df.loc[mask, variation_column])
+    print("creating plot")
+    fig, ax = plt.subplots()
+    ax.set_xlabel(x_axis)
+    ax.set_ylabel(y_axis)
+    for variation_value in df.loc[:, variation_column].unique()[::2]:
+        mask = df.loc[:, variation_column] == variation_value
+        df_reduced = df.loc[mask, :]
+        ax.plot(
+            df_reduced.loc[:, x_axis],
+            df_reduced.loc[:, y_axis],
+            marker="o",
+            ls=":",
+            lw=2,
+            label=f"{variation_column}={variation_value}",
+        )
+
+    return fig, ax
