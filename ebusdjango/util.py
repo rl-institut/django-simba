d22727d1d90372e66fd12bab4784eff0456fc594
PaulScheer <paul.scheer@rl-institut.de>
2025-06-11 16:19:13 +0200

    Implement encoding detection for SimbaScheduleReader

--- ebusdjango/util.py
+++ ebusdjango/util.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from django.conf import settings
+from charset_normalizer import from_path
 
 
 def get_static_file_path(package, path_from_static):
@@ -12,3 +13,22 @@ def get_static_file_path(package, path_from_static):
             p = Path(str(p).lstrip("/"))
         p = Path(settings.BASE_DIR, package, p)
     return p
+
+
+def get_file_encoding(filepath: str | Path) -> str | None:
+    """
+    Return the most likely encoding of a filepath or None.
+
+    If no match is found, the function returns None
+    :param filepath: str or Path of the file
+    :return: string of encoding or None if no encoding was found
+    """
+
+    result = from_path(filepath).best()
+    if result is None:
+        return None
+    if result.encoding == "utf_8":
+        # utf-8 does not strip bom from file.
+        # this breaks proper content reading.
+        return "utf-8-sig"
+    return result.encoding
