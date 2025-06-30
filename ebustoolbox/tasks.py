018ab27ec5f4b33260b9f9a35bd3e09b6bc237db
PaulScheer <paul.scheer@rl-institut.de>
2025-06-30 13:59:51 +0200

    Use generate_depot_layout instead of non working generate_optimal_depot_layout

--- ebustoolbox/tasks.py
+++ ebustoolbox/tasks.py
@@ -1652,7 +1652,7 @@ def run_eflips(task_id) -> None:
 
     # Constructing the database URL manually
     db_url = create_db_url()
-    generate_depot_optimal_size(
+    generate_depot_layout(
         db_scenario, database_url=db_url, charging_power=90, delete_existing_depot=True
     )
 
