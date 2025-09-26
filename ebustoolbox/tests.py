4685a298c88e860617ab40bbcc0c9cac47d19d92
PaulScheerRLI <104760879+PaulScheerRLI@users.noreply.github.com>
2025-09-26 14:50:19 +0200

    Add function to align socs between consecutive events (#269)
    * Add function to align socs between consecutive events
    
    * Align socs up to a depot charge event
    
    * Align events only up to a depot charge event
    
    * do not raise errors for cumulative delta soc
    consolidation now uses the delta soc of the original events for
    evaluation, while using the cumulative delta soc for fixing
    
    * Remove assertion error and instead write a error log
    
    * Overwrite soc_end value with value from depot validation
    
    * Fix test
    replace_event_timeseries now applies the timeseries it received with a
    error log. assert the error log is produced instead of an assertion
    error raised
    
    * Update ebustoolbox/tasks.py
    
    Co-authored-by: Moritz Schiel <82523186+mosc5@users.noreply.github.com>
    
    * Update ebustoolbox/tasks.py
    
    Co-authored-by: Moritz Schiel <82523186+mosc5@users.noreply.github.com>
    
    * Improve logs and abbreviate lists
    
    ---------
    
    Co-authored-by: Moritz Schiel <82523186+mosc5@users.noreply.github.com>

--- ebustoolbox/tests.py
+++ ebustoolbox/tests.py
@@ -484,10 +484,21 @@ class SimulationTestCase(TransactionTestCase):
         num_ts = len(event.timeseries["time"])
         # check start/end soc
         assert event.soc_start != event.soc_end
-        with self.assertRaises(AssertionError):
+        soc_end = event.soc_end
+        with self.assertLogs(logger="custom") as cm:
             tasks.replace_event_timeseries(event, [event.soc_start] * num_ts)
-        with self.assertRaises(AssertionError):
+            # Check if any log entry contains the substring
+            self.assertTrue(
+                any("Depot Charging Simulation diverged" in message for message in cm.output),
+                "Error log not found",
+            )
+        event.soc_end = soc_end
+        with self.assertLogs(logger="custom") as cm:
             tasks.replace_event_timeseries(event, [event.soc_end] * num_ts)
+            self.assertTrue(
+                any("Depot Charging Simulation diverged" in message for message in cm.output),
+                "Error log not found",
+            )
 
         # check length of timeseries
         assert num_ts > 2
