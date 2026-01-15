24f0acc98e9b0fefb62637fd94ac4212197138ac
PaulScheerRLI <104760879+PaulScheerRLI@users.noreply.github.com>
2026-01-15 12:55:14 +0100

    Fix Depot summary (#329)
    * Fix Depot summary
    
    * Added Manuelle Einstellungen as details/summary
    
    * get area_type in the translated form. rearange data

--- ebustoolbox/views.py
+++ ebustoolbox/views.py
@@ -1303,7 +1303,12 @@ class SummaryView(AuthorizedMixIn, TemplateView):
             is_electrified=False, is_electrifiable=True
         )
         context["excluded_stations"] = scenario_stations.filter(id__in=excluded_ids)
-        context["depot_wishes"] = DepotConfigurationWish.objects.filter(scenario=scenario)
+        context["depot_wishes"] = (
+            DepotConfigurationWish.objects.filter(scenario=scenario)
+            .prefetch_related("areainformation_set")
+            .prefetch_related("areainformation_set__vehicle_type")
+        )
+
         return context
 
     def post(self, request, *args, **kwargs):
