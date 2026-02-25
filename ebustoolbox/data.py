8057ec1c41e4765f613042c4bce59916f25a0308
0pss <71946340+0pss@users.noreply.github.com>
2026-02-25 10:28:39 +0100

    TCO plot works, translated

--- ebustoolbox/data.py
+++ ebustoolbox/data.py
@@ -1146,14 +1146,9 @@ def get_start_end_time(scenario: Scenario) -> tuple[datetime.datetime, datetime.
 
 def get_tco(task_id):
     try:
-        # Get the parent scenario
-        parent_scenario = Scenario.objects.get(task_id=task_id)
-
-        children = Scenario.objects.filter(parent_id=parent_scenario.id)
-        results = [c.tco_result for c in children]
-        for result in results:
-            print(result)
-        return {"tco_result": result}
+        s = Scenario.objects.get(task_id=task_id)
+        result = s.tco_result
+        return result
 
     except Scenario.DoesNotExist:
         return {"error": "Scenario or child scenario not found"}
